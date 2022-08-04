2fbc14d6273f6dc98a73f31b286182b1c4f4b3d6
Ruslan <ruslan_feoktistov@mail.ru>
2022-08-04 21:54:20 +0400

    наибольшее

---
+++ giri/Program.cs
@@ -0,0 +1,13 @@
+﻿int a=4;
+int b=9;
+int c=7;
+int d=2;
+int e=1;
+
+int max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+Console.Write("max=");
+Console.WriteLine(max);
