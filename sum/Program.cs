845266ffb197f061960eb31948ce245c0426111b
Ruslan <ruslan_feoktistov@mail.ru>
2022-08-04 20:59:44 +0400

    summ

--- sum/Program.cs
+++ sum/Program.cs
@@ -1,6 +1,8 @@
-﻿int numberA = 3;
-int numverB = 7;
-int result = numberA + numverB;
+﻿double numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+double result = numberA / numberB;
 Console.WriteLine(result);
 
     
