8e955cb4820e5c0388f63725b97422045840b9f9
Ruslan <ruslan_feoktistov@mail.ru>
2022-08-04 21:43:08 +0400

    маша

---
+++ del/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("О, шкура пришла");
+
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+
+}
